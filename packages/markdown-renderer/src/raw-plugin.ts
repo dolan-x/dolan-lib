718a51259e8dc2b65bc21af012d29ce3965c7165
so1ve <nn_201312@163.com>
2022-10-16 10:46:49 +0800

    fix: build error

--- packages/markdown-renderer/src/raw-plugin.ts
+++ packages/markdown-renderer/src/raw-plugin.ts
@@ -1,8 +1,8 @@
-import { visit } from "unist-util-visit";
 import type { Plugin } from "unified";
+import { visit } from "unist-util-visit";
 
 export const remarkDolanRaw: Plugin = () => {
-  return function transformer (tree) {
+  return (tree) => {
     visit(tree, "code", (node: any) => {
       if (node.lang === "dolan-raw") {
         node.type = "html";
